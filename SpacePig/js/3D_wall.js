d15ef507daf10eee38d4891470b6c511ab9ae869
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-08-02 12:47:16 +0900

    Update 3D_wall.js
    Use mousemove events to make space look like 3D places and add progress-bar codes.

--- SpacePig/js/3D_wall.js
+++ SpacePig/js/3D_wall.js
@@ -1,18 +1,36 @@
 (function() {
     const houseElem = document.querySelector('.house');
+    const stageElem = document.querySelector('.stage');
+    const progressElem = document.querySelector('.progress-bar');
+    const progressImgElem = document.querySelector('.progress-bar>img');
     let maxScroll;
+    let mousePos = {
+        x: 0,
+        y: 0
+    }
 
     function scrollHandler() {
-        const scollPer = pageYOffset / maxScroll;
-        const zMove = scollPer * 980 - 490;
+        const scrollPer = pageYOffset / maxScroll;
+        const zMove = scrollPer * 980 - 490;
         houseElem.style.transform = 'translateZ(' + zMove + 'vw)';
+
+        //progress-bar
+        progressElem.style.width = scrollPer * 100 + '%';
+        progressImgElem.style.left = scrollPer * 100 + '%';
     }
 
     function resizeHandler() {
         maxScroll = document.body.offsetHeight - window.innerHeight;
     }
 
+    function mouseMoveHandler(e) {
+        mousePos.x = (e.clientX / window.innerWidth - 0.5) * 30;
+        mousePos.y = -(e.clientY / window.innerHeight - 0.5) * 30;
+        stageElem.style.transform = 'rotateX(' + mousePos.y + 'deg) rotateY(' + mousePos.x + 'deg)'
+    }
+    window.addEventListener('mousemove', mouseMoveHandler);
     window.addEventListener('scroll', scrollHandler);
     window.addEventListener('resize', resizeHandler);
+
     resizeHandler();
 })();
