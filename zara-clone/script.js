bfa2e2ea65b8f34f0ab7aacdba156a101affde43
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-08-19 10:04:26 +0900

    Reset the data and add horizontal slide button click event listener.
    Change the way of definition of image data.
    And use button click event listeners to slide the page horizontally.

--- zara-clone/script.js
+++ zara-clone/script.js
@@ -9,39 +9,39 @@ const mainData = {
     man: [{
             title: 'NEW IN',
             contents: "Explore this week's latest menswear pieces of the season curated for you <br>Autumn Winter Man Collection",
-            image: "images/landscape_5.jpg"
+            number: 5
         }, {
             title: 'COLLECTION',
             contents: "Discover this week's pieces from our latest collection <br>Autumn Winter Man Collection",
-            image: "images/landscape_6.jpg"
+            number: 6
         }, {
             title: 'SHOES & BABS',
             contents: "Explore the new collection of Shoes & Bags <br>Autumn Winter Man Collection",
-            image: "images/landscape_7.jpg"
+            number: 7
         },
         {
             title: 'JOIN LIFE',
             contents: "Working on different programmes to reduce the environmental impact of our products",
-            image: "images/landscape_8.jpg"
+            number: 8
         }
     ],
     woman: [{
             title: 'NEW IN',
             contents: "Explore this week's latest womenswear pieces of the season curated for you<br>Autumn Winter Woman Collection",
-            image: "images/landscape_1.jpg"
+            number: 1
         }, {
             title: 'COLLECTION',
             contents: "Discover this week's pieces from our latest collection <br>Autumn Winter woman Collection",
-            image: "images/landscape_2.jpg"
+            number: 2
         }, {
             title: 'SHOES & BABS',
             contents: "Explore the new collection of Shoes & Bags <br>Autumn Winter woman Collection",
-            image: "images/landscape_3.jpg"
+            number: 3
         },
         {
             title: 'JOIN LIFE',
             contents: "Working on different programmes to reduce the environmental impact of our products",
-            image: "images/landscape_4.jpg"
+            number: 4
         }
     ],
 
@@ -52,16 +52,18 @@ const mainData = {
         }, {
             title: 'COLLECTION',
             contents: "Discover this week's pieces from our latest collection <br>Autumn Winter kids Collection",
-            image: "images/landscape_9.jpg"
+            number: 9
         }, {
             title: 'SHOES & BABS',
             contents: "Explore the new collection of Shoes & Bags <br>Autumn Winter kids Collection",
-            image: "images/landscape_10.jpg"
+            number: 10
+
         },
         {
             title: 'JOIN LIFE',
             contents: "Working on different programmes to reduce the environmental impact of our products",
-            image: "images/landscape_11.jpg"
+            number: 11
+
         }
     ]
 };
@@ -79,7 +81,7 @@ function ShowMainElem() {
             contentEl.classList.add('content');
             if (content.video) {
                 contentEl.innerHTML = `
-                    <div class="content" id="content">
+                    <div class="content">
                         <video id="main-video" src="${content.video}" autoplay></video>
                         <h3>${content.title}</h3>
                         <p>${content.contents}</p>
@@ -88,8 +90,8 @@ function ShowMainElem() {
                     </div>`
             } else {
                 contentEl.innerHTML = `
-                    <div class="content" id="content">
-                        <img id="main-img" src="${content.image}" alt="">
+                    <div class="content" id="${content.number}">
+                        <img id="main-img" src="images/landscape_${content.number}.jpg" alt="">
                         <h3>${content.title}</h3>
                         <p>${content.contents}</p>
                         <button class="btn view-btn" id="${content.title.toLowerCase()}">VIEW</button>
@@ -102,9 +104,52 @@ function ShowMainElem() {
     }
 };
 
+
+
+
 ShowMainElem();
 
+// Button click
+let currentActivePage = 1;
+const horiPage = document.querySelectorAll('.contents-column');
+const leftBtn = document.getElementById('left');
+const rightBtn = document.getElementById('right');
 
+// Inital active page
+horiPage[currentActivePage].className = 'contents-column active';
+horiPage[currentActivePage - 1].className = 'contents-column left';
+horiPage[currentActivePage + 1].className = 'contents-column right';
+
+// Right button click event 
+rightBtn.addEventListener('click', () => {
+
+    horiPage[currentActivePage].className = 'contents-column left';
+
+    currentActivePage = currentActivePage + 1;
+
+    if (currentActivePage >= horiPage.length) {
+        currentActivePage = horiPage.length - 1
+    }
+
+    horiPage[currentActivePage].className = 'contents-column active';
+
+    // ShowhoriSlideBtn();
+})
+
+// Left button click event 
+leftBtn.addEventListener('click', () => {
+    horiPage[currentActivePage].className = 'contents-column right';
+
+    currentActivePage = currentActivePage - 1;
+
+    if (currentActivePage < 0) {
+        currentActivePage = 0
+    }
+
+    horiPage[currentActivePage].className = 'contents-column active';
+
+    // ShowhoriSlideBtn();
+})
 
 
 // Change image by window size
