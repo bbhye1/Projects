fffbd53087255c22237741f2c8b38fe8286355bd
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-09-02 15:29:25 +0900

    Update scroll-section-2 : right video images, center image.
    Fix some values of right video imges and display center image by scrolling.

--- simmons/js/main.js
+++ simmons/js/main.js
@@ -65,34 +65,47 @@
             title: document.querySelector('#scroll-section-2 .title'),
             text: document.querySelector('.pocket-spring-elem'),
             pin: document.querySelector('.pin'),
-            canvas: document.querySelector('#scroll-canvas-2'),
-            context: document.querySelector('#scroll-canvas-2').getContext('2d'),
-            videoImages: [],
+            canvas1: document.querySelector('#scroll-canvas-2-1'),
+            context1: document.querySelector('#scroll-canvas-2-1').getContext('2d'),
+            videoImages1: [],
+
+            canvas2: document.querySelector('#scroll-canvas-2-2'),
+            context2: document.querySelector('#scroll-canvas-2-2').getContext('2d'),
+            canvas2Image: []
         },
         values: {
             videoFrame: 428,
-            videoSequence: [0, 427, { start: 0.25, end: 0.6 }],
+            videoSequence: [0, 427, { start: 0.2, end: 0.65 }],
             videoOpacityIn: [0, 1, { start: 0.2, end: 0.3 }],
-            videoOpacityOut: [1, 0, { start: 0.9, end: 0.95 }],
-            videoTranslateYOut: [0, -20, { start: 0.8, end: 0.9 }],
+            videoOpacityOut: [1, 0, { start: 0.85, end: 0.95 }],
+            videoScaleOut: [0, 0, { start: 0.65, end: 0.7 }],
 
-            titleTranslateYIn: [20, 0, { start: 0.1, end: 0.2 }],
-            titleTranslateYOut: [0, -20, { start: 0.35, end: 0.4 }],
-            titleOpactiytIn: [0, 1, { start: 0.1, end: 0.2 }],
-            titleOpactiytOut: [1, 0, { start: 0.35, end: 0.4 }],
+
+            imageTranslateX: [-150, -50, { start: 0.7, end: 0.85 }],
+            imageScale: 0,
+
+            titleTranslateYIn: [20, 0, { start: 0.05, end: 0.15 }],
+            titleTranslateYOut: [0, -20, { start: 0.45, end: 0.55 }],
+            titleOpactiytIn: [0, 1, { start: 0.05, end: 0.15 }],
+            titleOpactiytOut: [1, 0, { start: 0.45, end: 0.55 }],
 
             textTranslateYIn: [20, 0, { start: 0.15, end: 0.25 }],
             textOpactiytIn: [0, 1, { start: 0.15, end: 0.25 }],
 
-            pinWidth: [10, 100, { start: 0.25, end: 0.35 }],
+            pinWidth: [10, 100, { start: 0.25, end: 0.45 }],
 
 
         }
     }, {
         heightNum: 5,
         scrollHeight: 0,
-        objs: { scene: document.querySelector('#scroll-section-3'), },
-        values: {}
+        objs: {
+            scene: document.querySelector('#scroll-section-3'),
+            prevCanvas: document.querySelector('#scroll-canvas-2-2'),
+        },
+        values: {
+            prevCanvasOpacity: [1, 0, { start: 0.1, end: 0.2 }],
+        }
     }, {
         heightNum: 1,
         scrollHeight: 0,
@@ -110,17 +123,15 @@
 
         const heightRatio = window.innerHeight / 1080;
         const widthRatio = window.innerWidth / 1920;
-
         if (window.innerHeight > window.innerWidth) {
             sceneInfo[1].objs.canvas.style.transform = `translateX(-50%) scale(${heightRatio})`;
-            sceneInfo[2].objs.canvas.style.transform = `translateX(-50%) scale(${widthRatio*0.85})`;
-
         } else {
             sceneInfo[1].objs.canvas.style.transform = `translateX(-50%) scale(${widthRatio})`;
-            sceneInfo[2].objs.canvas.style.transform = `translateX(-50%) scale(${heightRatio*0.85})`;
         }
-
-
+        sceneInfo[2].objs.canvas1.style.transform = `scale(${heightRatio})`;
+        sceneInfo[2].values.videoScaleOut[0] = heightRatio;
+        sceneInfo[2].objs.canvas2.style.transform = ` translateX(-150%) scale(${heightRatio*0.85})`;
+        sceneInfo[2].values.imageScale = heightRatio * 0.85;
 
     }
 
@@ -134,6 +145,7 @@
         values.titleHeight[2].start = values.titleTop[2].start;
         values.titleHeight[2].end = values.titleHeight[2].start + (objs.titleWhite.offsetHeight / sceneInfo[currentScene].scrollHeight);
         values.titleHeight[0] = objs.titleWhite.offsetHeight;
+
     }
 
 
@@ -218,12 +230,11 @@
             case 2:
                 if (enterNewScene) return;
                 const sequenceIndex2 = Math.round(calcValues(values.videoSequence, currentYOffset));
-                const heightRatio = window.innerHeight / 1080;
-                const canvasVideoXPos1 = objs.canvas.offsetWidth * heightRatio * 0.85;
-                objs.context.drawImage(objs.videoImages[sequenceIndex2], canvasVideoXPos1, 0);
-
-
-
+                values.videoScaleOut[1] = values.videoScaleOut[0] * 0.85;
+                objs.context1.drawImage(objs.videoImages1[sequenceIndex2], 0, 0);
+                objs.canvas1.style.transform = `scale(${calcValues(values.videoScaleOut, currentYOffset)})`;
+                objs.context2.drawImage(objs.canvas2Image[0], 0, 0);
+                objs.canvas2.style.transform = `translateX(${Math.round(calcValues(values.imageTranslateX,currentYOffset))}%) scale(${values.imageScale})`;
 
                 if (scrollRatio <= 0.3) {
                     objs.title.style.opacity = calcValues(values.titleOpactiytIn, currentYOffset);
@@ -232,7 +243,7 @@
                     objs.text.style.transform = `translateY(${calcValues(values.textTranslateYIn, currentYOffset)}px)`;
                     objs.pin.style.width = `${calcValues(values.pinWidth, currentYOffset)}px`;
 
-                    objs.canvas.style.opacity = calcValues(values.videoOpacityIn, currentYOffset);
+                    objs.canvas1.style.opacity = calcValues(values.videoOpacityIn, currentYOffset);
                 } else {
                     objs.title.style.opacity = calcValues(values.titleOpactiytOut, currentYOffset);
                     objs.text.style.opacity = calcValues(values.titleOpactiytOut, currentYOffset);
@@ -240,9 +251,12 @@
                     objs.text.style.transform = `translateY(${calcValues(values.titleTranslateYOut, currentYOffset)}px)`;
                     objs.pin.style.width = `${calcValues(values.pinWidth, currentYOffset)}px`;
 
-                    objs.canvas.style.opacity = calcValues(values.videoOpacityOut, currentYOffset);
+                    objs.canvas1.style.opacity = calcValues(values.videoOpacityOut, currentYOffset);
                 }
-
+                break;
+            case 3:
+                if (enterNewScene) return;
+                objs.prevCanvas.style.opacity = calcValues(values.prevCanvasOpacity, currentYOffset);
                 break;
         }
     }
@@ -258,9 +272,12 @@
         for (let i = 0; i < sceneInfo[2].values.videoFrame; i++) {
             const image = new Image();
             image.src = `../video/003/${i+1}.jpg`;
-            sceneInfo[2].objs.videoImages.push(image);
+            sceneInfo[2].objs.videoImages1.push(image);
         }
 
+        const canvasImage = new Image();
+        canvasImage.src = '../images/img-bed.jpg'
+        sceneInfo[2].objs.canvas2Image.push(canvasImage);
     }
 
 
@@ -300,10 +317,11 @@
         setLayout();
         drawCanvasImages();
 
-    })
+    });
     window.addEventListener('resize', () => {
+        setLayout();
         drawCanvasImages();
-    })
+    });
     setLayout();
     setInfoValues();
 
